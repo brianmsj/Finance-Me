ebd8805b3456ff16e49178b9c4233a0a9426b741
Brian McMinn <brianmsj@gmail.com>
2017-10-06 10:17:18 -0700

    html page is rendering with the server running in development

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import thunk from 'redux-thunk';
+import './main.css';
+
+ReactDOM.render(
+    <App />
+    ,
+  document.getElementById('root')
+);
---
+++ server/index.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const http = require('http');
+const mongoose = require('mongoose');
+
+
+
+mongoose.Promise = global.Promise
+let secret = {
+  DATABASE_URL: process.env.DATABASE_URL
+};
+
+if(process.env.NODE_ENV != 'production') {
+  secret = require('./secret');
+}
+global.secret = secret
+
+const app = express();
+app.use(bodyParser.json());
+console.log("connecting")
+
+
+// Serve the built client
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// Unhandled requests which aren't for the API should serve index.html so
+// client-side routing using browserHistory can function
+app.get(/^(?!\/api(\/|$))/, (req, res) => {
+    const index = path.resolve(__dirname, '../client/build', 'index.html');
+    res.sendFile(index);
+});
+
+let server;
+function runServer(port=3001) {
+    return new Promise((resolve, reject) => {
+        mongoose.connect(secret.DATABASE_URL || process.env.DATABASE_URL, err => {
+            if(err) {
+              return reject(err);
+            }
+            console.log("Db Connected");
+            server = app.listen(port, () => {
+              resolve();
+            }).on('error', reject);
+        });
+    });
+}
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
+
+module.exports = {
+    app, runServer, closeServer, secret
+};
