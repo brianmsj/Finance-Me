785f103da1bfa5d21394ec3e642e1f25d92cfaf4
Brian McMinn <brianmsj@gmail.com>
2017-10-07 07:46:20 -0700

    added schmea with API Response and Bcrypt password encryption

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
-  userName: String,
+  userName: {type: String, required: true, unique: true},
   email: String,
-  password: String
+  password: {type: String, required: true}
 })
+
+UserSchema.methods.apiRepr = function() {
+    return {
+        username: this.username || '',
+        firstName: this.firstName || '',
+        lastName: this.lastName || ''
+    };
+};
+UserSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+};
+const User = mongoose.model('User', UserSchema);
+
+module.exports = {User};
