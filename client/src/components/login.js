dce5e2caaf985af059aa778d66d86226ee866fba
Brian McMinn <brianmsj@gmail.com>
2017-10-07 17:09:22 -0700

    password and username checks are working

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -10,10 +10,14 @@ class Login extends React.Component {
         this.state = {
           userNameError: true,
           passwordError: true,
+          signUpPasswordError: true,
+          chooseUserNameError: true
         };
     this.checkUserName = this.checkUserName.bind(this);
     this.checkPassword = this.checkPassword.bind(this);
     this.onLoginSubmit = this.onLoginSubmit.bind(this);
+    this.choosePassword = this.choosePassword.bind(this);
+    this.chooseUserName = this.chooseUserName.bind(this);
     }
     checkUserName(event){
     if(this.userName.value.length < 6) {
@@ -35,7 +39,20 @@ class Login extends React.Component {
       var usernombre = this.userName.value
       var passWorde = this.password.value
       this.props.dispatch(actions.login(usernombre,passWorde));
-
+    }
+    chooseUserName(event) {
+      console.log(this.chooseUser.value)
+      if(this.chooseUser.value.length < 6) {
+         this.setState({chooseUserNameError: false});
+      }
+      else {
+         this.setState({chooseUserNameError: true});
+      }}
+    choosePassword(event) {
+      let reg = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/
+      let pass = this.signUpPassword.value
+      let acceptable = reg.test(pass)
+      this.setState({signUpPasswordError:acceptable})
     }
     // validateEmail(value) {
     // var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -45,9 +62,11 @@ class Login extends React.Component {
     render() {
         let hidden = this.state.userNameError ? 'hidden': '';
         let pwHidden = this.state.passwordError ? 'hidden': '';
+        let signUpPwHidden = this.state.signUpPasswordError ? 'hidden': '';
+        let chooseUserT = this.state.chooseUserNameError ? 'hidden': '';
         return (
           <div>
-          <div className="modal fade" id="flipFlop" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+          <div className="modal fade" id="flipFlop" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
             <div className="modal-content">
             <div className="modal-header">
@@ -90,7 +109,7 @@ class Login extends React.Component {
           </div>
           </div>
           </div>
-          <div className="modal fade" id="second" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+          <div className="modal fade" id="second" tabIndex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
             <div className="modal-dialog" role="document">
             <div className="modal-content">
             <div className="modal-header">
@@ -103,27 +122,55 @@ class Login extends React.Component {
             <form>
               <div className='form-group'>
               <label htmlFor='firstname'>First Name</label>
-              <input id='firstname' className='form-control' type='text'/>
+              <input
+                id='firstname'
+                className='form-control'
+                ref={ref => this.firstname = ref}
+                type='text'/>
               </div>
               <div className='form-group'>
               <label htmlFor='lastname'>Last Name</label>
-              <input id='lastname' className='form-control' type='text'/>
+              <input
+                id='lastname'
+                className='form-control'
+                ref={ref => this.lastname = ref}
+                type='text'/>
               </div>
               <div className='form-group'>
               <label htmlFor='chooseUser'>Choose Username</label>
-              <input id='chooseUser' className='form-control' type='text'/>
+              <input
+                id='chooseUser'
+                ref={ref => this.chooseUser = ref}
+                onChange={this.chooseUserName}
+                className='form-control'
+                type='text'/>
+              <span className={`alert-danger ${chooseUserT}`}>Username must be 6 characters</span>
               </div>
               <div className='form-group'>
               <label htmlFor='email'>E-mail</label>
-              <input id='email' className='form-control' type='text'/>
+              <input
+                id='email'
+                ref={ref => this.email = ref}
+                className='form-control'
+                type='text'/>
               </div>
               <div className='form-group'>
               <label htmlFor='signUpPassword'>Password</label>
-              <input id='signUpPassword' className='form-control' type='text'/>
+              <input
+                id='signUpPassword'
+                ref={ref => this.signUpPassword = ref}
+                onChange={this.choosePassword}
+                className='form-control'
+                type='text'/>
+              <span className={`alert-danger ${signUpPwHidden}`}>Password must contain 8 char, 1 capital letter, 1 number, 1 special char @#$%^&* </span>
               </div>
               <div className='form-group'>
               <label htmlFor='reTypePassword'>Re-Type Password</label>
-              <input id='reTypePassword' className='form-control' type='text'/>
+              <input
+                id='reTypePassword'
+                ref={ref => this.reTypePassword = ref}
+                className='form-control'
+                type='text'/>
               </div>
               <button type='button' className='btn btn-primary'>Sign Up</button>
             </form>
