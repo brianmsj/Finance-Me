bb1c9f5b3fe4287300b9acab0d9e9301517e2529
Brian McMinn <brianmsj@gmail.com>
2017-10-15 19:19:43 -0700

    created budget card

--- client/src/components/budgetCard.js
+++ client/src/components/budgetCard.js
@@ -9,19 +9,28 @@ class BudgetCard extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
+        showInput: false,
       };
+    this.changeToInput = this.changeToInput.bind(this)
+    this.printHello = this.printHello.bind(this)
+    }
+    changeToInput() {
+      this.setState({showInput: !this.state.showInput})
 
+    }
+    printHello() {
+      return 'hello world'
     }
 
 
-
     render() {
+      const categories = ['Savings','Housing','Transportation']
+      let showInput;
+      if(!this.state.showInput) {showInput=<p id='divone' onClick={this.changeToInput}></p>}
+      else {showInput=<input autoFocus onBlur={this.changeToInput} placeholder='div ONE' type='text'></input>}
       return (
       <div className='card-container'>
-       <div className='card-title'>This is a card</div>
-       <BudgetRow />
-       <BudgetRow />
-       <BudgetRow />
+        {showInput}
        <BudgetRow />
       </div>
 
--- client/src/components/budgetLeftContainer.js
+++ client/src/components/budgetLeftContainer.js
@@ -2,9 +2,11 @@ import React from 'react';
 import * as actions from '../actions/index';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import BudgetCard from './budgetCard'
+import BudgetRow from './budgetRow'
 
 
-class BudgetRow extends React.Component {
+class BudgetLeftContainer extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
@@ -16,11 +18,7 @@ class BudgetRow extends React.Component {
 
     render() {
       return (
-      <div className='card-container'>
-      <div className='inline-budget-row'>
-       <input type='text' placeholder='budget item'></input>
-       <input className='inline-budget-second' type='text' placeholder='budget item'></input>
-      </div>
+      <div>
       </div>
 
     )
@@ -31,4 +29,4 @@ class BudgetRow extends React.Component {
 const mapStateToProps = (state, props) => ({
 })
 
-export default connect(mapStateToProps)(BudgetRow)
+export default connect(mapStateToProps)(BudgetLeftContainer)
